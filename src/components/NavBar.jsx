6ada7768c8d33d9db564cb979b12c333ba25bd09
Mariano Eiro <marianoeiro@hotmail.com>
2022-09-09 00:43:04 -0300

    Enlaces con navegacion primaria

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,14 +4,14 @@ const NavBar = () => {
   return (
     <div className="p-2 border-b shadow-sm flex justify-between items-center bg-orange-100">
         <div className='container mx-auto ml-8 flex items-center gap-4'>
-            <img className='rounded-full' width={100} src={logo} alt="Logo de Pichonos"/>
+            <a href='/'><img className='rounded-full' width={100} src={logo} alt="Logo de Pichonos"/></a>
             <h2 className=''>PICHONO'S</h2>
         </div>
         <nav className='flex'>
-            <ul className='flex mr-20 gap-5 cursor-pointer' >
-                <li><strong>Home</strong></li>
-                <li><strong>Clothing</strong></li>
-                <li><strong>Accesories</strong></li>
+            <ul className='flex mr-20 gap-5' >
+                <li><a href='/'><strong>Home</strong></a></li>
+                <li><a href='/'><strong>Clothing</strong></a></li>
+                <li><a href='/'><strong>Accesories</strong></a></li>
             </ul>
         </nav>
     </div>
