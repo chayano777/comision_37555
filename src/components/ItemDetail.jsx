c0cfe27f625bdb588fcddd91426d4365dd4446d9
meiro <mariano.ricardo.eiro@gmail.com>
2022-09-25 22:52:53 -0300

    cambios en el CSS del ItemDetail

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -4,10 +4,10 @@ const ItemDetail = ({ idProd }) => {
   console.log(idProd);
   return (
     <div className="my-10 mx-3 w-1/2 h-1/2 border rounded shadow-md bg-orange-100 overflow-hidden transition-property: all duration-100 ease-in min-h-280">
-      <div className="flex justify-evenly">
-        <div className="my-5 w-72 h-64 object-cover">
+      <div className="flex justify-evenly border-2 border-orange-700 border-solid rounded-xl">
+        <div className="mx-5 my-5 w-72 h-64 object-cover">
           <img
-            className="py-5 w-full h-full object-contain"
+            className="border-2 border-gray-200 border-solid rounded-xl w-full h-full"
             src={idProd.imagen}
             alt=""
           ></img>
