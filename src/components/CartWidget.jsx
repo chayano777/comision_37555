6ff58a71f7b7538d8ed9f3007c703f72538ef9f5
meiro <mariano.ricardo.eiro@gmail.com>
2022-10-07 14:40:34 -0300

    Se realizo un fix de visualizacion cuando el cartWidget no tiene productos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -7,15 +7,17 @@ const CartWidget = () => {
 
   return (
     <div className="flex">
-      <div className="flex relative ml-5">
+      <div className="flex relative ml-5 mr-10">
         <div className="">
-          <BsCart4 className="text-orange-900 text-2xl ml-2 mr-5" />
+          <BsCart4 className="text-orange-900 text-2xl ml-2 mr-4" />
         </div>
         {qtyProd !== 0 ? (
           <div className="absolute -top-2 -right-0 bg-orange-700 rounded-full h4 w-4 font-medium text-xs text-center text-white">
             <p>{qtyProd}</p>
           </div>
-        ) : null}
+        ) : <div className="absolute -top-2 -right-0 bg-orange-700 rounded-full h4 w-4 font-medium text-xs text-center text-white">
+              <p>0</p>          
+          </div>}
       </div>
     </div>
   );
