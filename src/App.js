e9828e60c78444892dd6b6b4ccf39c20e017552e
meiro <mariano.ricardo.eiro@gmail.com>
2022-10-07 00:44:15 -0300

    Se creo el context

--- src/App.js
+++ src/App.js
@@ -4,22 +4,30 @@ import Cart from "./components/Cart";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import ItemListContainer from "./components/ItemListContainer";
 import NavBar from "./components/NavBar";
-
+import CartProvider from "./context/CartContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<ItemListContainer greeting="Bienvenidos a Pichono's" />}
-        />
-        <Route path="/category/:categoryN" element={<ItemListContainer greeting="Bienvenidos a Pichono's" />}
-        />
-        <Route path="/category/:categoryN/:id" element={<ItemDetailContainer />}
-        />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={<ItemListContainer greeting="Bienvenidos a Pichono's" />}
+          />
+          <Route
+            path="/category/:categoryN"
+            element={<ItemListContainer greeting="Bienvenidos a Pichono's" />}
+          />
+          <Route
+            path="/category/:categoryN/:id"
+            element={<ItemDetailContainer />}
+          />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
